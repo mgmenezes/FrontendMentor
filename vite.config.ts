b5f65039bf1e9fcd8ea04284f3d59caa2164592e
Moises Menezes <moisesmenezes53@gmail.com>
2025-10-06 00:12:40 -0300

    setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// Detecta repo para base do GitHub Pages quando buildar no GitHub Actions
+const repository = process.env.GITHUB_REPOSITORY || '' // ex: "usuario/repositorio"
+const repoName = repository.includes('/') ? repository.split('/')[1] : repository
+const isUserSite = repoName.endsWith('.github.io')
+const isCI = !!process.env.GITHUB_ACTIONS
+const base = isCI ? (isUserSite ? '/' : `/${repoName}/`) : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
 })
